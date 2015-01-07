e2dc31dddc6f3c858dc075c071149102c265e13f
David-Hanna <dhanna@upei.ca>
2015-01-07 10:51:48 -0400

    Updating compiler output directories
    Directory structure change. Only one more directory change will occur
    inside src later to organize the code. Also a simple change in main.cpp
    based on a new c++ system function I learned today.

--- dev/Code/src/Main/main.cpp
+++ dev/Code/src/Main/main.cpp
@@ -4,6 +4,6 @@
 int main(int argc, void** argv)
 {
 	std::cout << "Hello, World!" << std::endl;
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	system("pause");
 	return 0;
 }
