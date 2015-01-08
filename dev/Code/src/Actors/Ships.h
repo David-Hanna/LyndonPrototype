39aef9b238992cecadde114afc9df3ab53270603
David-Hanna <dhanna@upei.ca>
2015-01-08 00:01:38 -0400

    Libraries and new files
    Adding SFML and tinyxml libraries, Ships.h, ShipParser.h, Ships.xml

--- dev/Code/src/Actors/Ships.h
+++ dev/Code/src/Actors/Ships.h
@@ -3,3 +3,24 @@
 //-------------------------------------------------------------------------------------------------------
 // Ships
 //-------------------------------------------------------------------------------------------------------
+
+#include <string>
+#include <map>
+
+#include "..\Scripting\Code\ShipParser.h"
+
+class BasicShip
+{
+protected:
+	std::string name;
+	std::map<std::string, std::string> attributes;
+	std::string imageFileName;
+
+public:
+	BasicShip() : name(""), imageFileName("") {}
+
+
+	std::string Name() const { return name; }
+	std::string TryGetAttribute(std::string attribName) { if (attributes.find(attribName) == attributes.end()) return NULL; else return attributes[attribName]; }
+	std::string ImageFileName() const { return imageFileName; }
+};
