b9ffa56b372453bc013bbf678a27787095617627
Shayne Walker <shayne_walker@live.ca>
2015-01-07 21:28:55 -0400

    main.cpp change
    Changes main.cpp to have graphical window

--- dev/Code/src/Main/main.cpp
+++ dev/Code/src/Main/main.cpp
@@ -1,9 +1,20 @@
 #include <limits>
 #include <iostream>
+#include "SFML/Graphics.hpp"
 
 int main(int argc, void** argv)
 {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
+
+	sf::RenderWindow window( sf::VideoMode ( 600, 600 ), "SFML WORKS!" );
+
+	while ( window.isOpen())
+	{
+		window.clear();
+
+		window.display();
+	}
+
+	//std::cout << "Hello, World!" << std::endl;
+	//system("pause");
 	return 0;
 }
